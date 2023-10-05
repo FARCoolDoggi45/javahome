db65e7710dc85b52ccad2e625496f84523258984
HomeLander <110926173+FARCoolDoggi45@users.noreply.github.com>
2023-10-05 22:25:49 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const data = [{
+    title: "Apex legend",
+    rating: 8.0,
+    systemrequiRements: "Medium",
+    platforms: "PC, Play station, Xbox",
+}];
+for (data of data) {
+    console.log(data);
+}
